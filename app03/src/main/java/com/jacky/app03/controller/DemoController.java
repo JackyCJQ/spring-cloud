cf4f08f3469d9a015f47afd409b6b131c8eee804
chenjiaqi <chenjiaqi@mobike.com>
2020-06-30 23:30:01 +0800

    code

--- app03/src/main/java/com/jacky/app03/controller/DemoController.java
+++ app03/src/main/java/com/jacky/app03/controller/DemoController.java
@@ -1,7 +1,6 @@
 package com.jacky.app03.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DemoController {
-    @RequestMapping(value = "/order",method = RequestMethod.POST)
+    @RequestMapping(value = "/order")
     public String demo(@RequestParam(value = "orderId", required = false) String orderId) {
         return "Hello World";
     }
